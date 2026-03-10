72c27b9187ee8e11a0632a03105a991761315140
Barta Daniel Zsolt <22684945+Dani05@users.noreply.github.com>
2026-03-10 17:58:37 +0100

    add DB

--- web-image-backend/src/main/java/org/example/webimagebackend/persistence/entity/ImageEntity.java
+++ web-image-backend/src/main/java/org/example/webimagebackend/persistence/entity/ImageEntity.java
@@ -1,15 +1,20 @@
 package org.example.webimagebackend.persistence.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "images")
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
